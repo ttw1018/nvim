269572aa1714e3b1599227e824b13999f8d46c52
Tiwent <627747903@qq.com>
2025-04-03 19:28:42 +0800

    add: support auto tag pair

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,40 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  version = "*",
-  event = "BufRead",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      auto_install = true,
-      sync_install = true,
-      highlight = {
-        enable = true,
-        user_language_tree = true,
-        disable = function(_, buf)
-          local max_filesize = 50 * 1024 * 1024 -- 100 MB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-        additional_vim_regex_highlighting = false,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = "*",
+    event = "BufRead",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        auto_install = true,
+        sync_install = true,
+        highlight = {
+          enable = true,
+          user_language_tree = true,
+          disable = function(_, buf)
+            local max_filesize = 50 * 1024 * 1024 -- 100 MB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = { "vue", "tsx", "jsx", "html" },
+    opts = {
+      opts = {
+        -- Defaults
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = true,
       },
-      indent = {
-        enable = true,
-      },
-    })
-  end,
+    },
+  },
 }
